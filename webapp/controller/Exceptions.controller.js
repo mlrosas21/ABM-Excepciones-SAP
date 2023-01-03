704f52dee9390add16f21541b3339ce76d3ee907
SE38606 grupo.ypf.com <SE38606@grupo.ypf.com>
2023-01-03 17:59:41 +0000

    Creation dialog

--- webapp/controller/Exceptions.controller.js
+++ webapp/controller/Exceptions.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, MessageBox, Fragment) {
         "use strict";
 
         return Controller.extend("ypf.zparamfreegd0.controller.Exceptions", {
@@ -24,9 +26,11 @@ sap.ui.define([
                 console.log("EDIT")
                 this.toggleEditMode(true)
             },
+            
             onCancel: function(){
                 console.log("CANCEL")
                 this.toggleEditMode(false)
+                this.getView().getModel().resetChanges();
             },
             toggleEditMode: function(bool){
                 switch(bool){
@@ -39,6 +43,58 @@ sap.ui.define([
                         this.getView().getModel("viewModel").setProperty("/editMode", false)
                         break;
                 }
+            },
+            onAdd: function(){
+                let oDeferredGroups = this.getView().getModel().getDeferredGroups();
+                if (this._oDialogCreacion) {
+                    this._oDialogCreacion.destroy();
+                }
+                this._onOpenDialog()
+            },
+            _onOpenDialog: function(){
+                Fragment.load({
+                    name: "ypf.zparamfreegd0.view.fragments.DialogCreation",
+                    controller: this,
+                    id: this.getView().getId()
+                }).then(function(oPopUp) {
+                    this._oDialog = oPopUp;
+                    this.getView().addDependent(oPopUp);
+                    this._oDialog.open()
+                }.bind(this))
+            },
+            onCrearExcepcion: function(){
+                MessageBox.confirm("Los cambios se asentarán en la base de datos. ¿Desea continuar?", {
+                    actions: ["Confirmar", MessageBox.Action.CANCEL],
+                    emphasizedAction: "Confirmar",
+                    onClose: function (sAction) {
+                        if (sAction === "Confirmar") {
+                            that.getView().getModel().submitChanges({
+                                success: function () {
+                                    that._oDialogCreacion.setBusy(false);
+                                    sap.m.MessageToast.show("Se han guardado los cambios");
+                                    that.getView().getModel().setDeferredGroups(ogetDeferredGroups);
+                                },
+                                error: function () {
+                                    that._oDialogCreacion.setBusy(false);
+                                    sap.m.MessageToast.show("Se producido un error, intente nuevamente");
+                                    that.getView().getModel().setDeferredGroups(ogetDeferredGroups);
+                                }
+                            });
+                            that._oDialogCreacion.close();
+                            that._oDialogCreacion.destroy();
+                        } else {
+                            that.getView().getModel().deleteCreatedEntry(oContext); // borrar entrada temporal
+                            that.getView().getModel().resetChanges();
+                            that.getView().getModel().setDeferredGroups(ogetDeferredGroups);
+                            that._oDialogCreacion.close();
+                            that._oDialogCreacion.destroy();
+                        }
+                    }
+                });
+            },
+            onDialogClose: function(){
+                this._oDialog.close();
+                this._oDialog.destroy();
             }
         });
     });
