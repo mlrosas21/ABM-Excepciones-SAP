0e826e5c475afb03ba90082ce06f0af623a785cc
SE38606 grupo.ypf.com <SE38606@grupo.ypf.com>
2023-01-03 16:20:53 +0000

    Table structure

--- webapp/controller/Exceptions.controller.js
+++ webapp/controller/Exceptions.controller.js
@@ -1,15 +1,44 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("ypf.zparamfreegd0.controller.Exceptions", {
             onInit: function () {
+                const oViewModel = new JSONModel({
+					tableNoDataText : "No DATA",
+					busy: true,
+					delay: 0,
+					totalItems: 0,
+					editMode: false
+				})
+                this.getView().setModel(oViewModel, "viewModel");
 
+            },
+            onEdit: function(){
+                console.log("EDIT")
+                this.toggleEditMode(true)
+            },
+            onCancel: function(){
+                console.log("CANCEL")
+                this.toggleEditMode(false)
+            },
+            toggleEditMode: function(bool){
+                switch(bool){
+                    case true:
+                        this.byId("idExceptionsTable").setMode(sap.m.ListMode.MultiSelect)
+                        this.getView().getModel("viewModel").setProperty("/editMode", true) 
+                        break;
+                    case false:
+                        this.byId("idExceptionsTable").setMode(sap.m.ListMode.None)
+                        this.getView().getModel("viewModel").setProperty("/editMode", false)
+                        break;
+                }
             }
         });
     });
